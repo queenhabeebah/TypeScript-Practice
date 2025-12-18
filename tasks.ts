a16afdc8fde2c225fb55d98166dd4ba085172385
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-12-18 19:30:50 +0200

    Submit: Task 1

---
+++ tasks.ts
@@ -0,0 +1,23 @@
+// Task 1
+interface SuccessResponse<T> {
+  status: "success";
+  data: T;
+}
+
+interface ErrorResponse {
+  status: "error";
+  message: "string";
+}
+
+type ApiResponse<T> = SuccessResponse <T> | ErrorResponse
+
+interface User {
+    name: string
+    userId: number
+}
+
+interface Admin {
+    adminId: number
+}
+
+type AdminUser = User & Admin
